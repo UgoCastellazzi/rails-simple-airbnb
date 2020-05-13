649c44ec7e1533e54375720063f5622824e8d74b
UgoCastellazzi <ugo.castellazzi@gmail.com>
2020-05-13 16:58:49 +0200

    Trying to add JS

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,13 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+// const form = document.querySelector("form");
+// const searchQuery = document.getElementById("search");
+// console.log(searchQuery);
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const searchQuery = document.getElementById("search").value;
+//   console.log(searchQuery);
+// });
