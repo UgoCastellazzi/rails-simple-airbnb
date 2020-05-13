d6a875b9015325ab0f6e2cc5351e7c9423a10428
UgoCastellazzi <ugo.castellazzi@gmail.com>
2020-05-13 16:32:53 +0200

    Ex 3 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Amazing duplex in Cali',
+  address: '42 Sunset Boulevard',
+  description: 'Large swimming pool where you can drown in black and white',
+  price_per_night: 300,
+  number_of_guests: 10
+)
+
+Flat.create!(
+  name: 'Small shared bedroom',
+  address: '221B Baker Street, London',
+  description: 'Nice roommate, need to be smoker-friendly',
+  price_per_night: 15,
+  number_of_guests: 1
+)
